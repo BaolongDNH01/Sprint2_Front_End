8b1a87460bd0a459fb95a5a00e4f7b1d0b0b2134
doquockhanh <khanhquocdo.qt@gmail.com>
2020-10-02 11:52:23 +0700

    khanh fix search suggest

--- src/app/product-list/product-manage/product-manage.component.ts
+++ src/app/product-list/product-manage/product-manage.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {isElementScrolledOutsideView} from '@angular/cdk/overlay/position/scroll-clip';
+import {computeStartOfLinePositions} from '@angular/compiler-cli/ngcc/src/sourcemaps/source_file';
 
 @Component({
   selector: 'app-product-manage',
@@ -6,8 +8,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./product-manage.component.css']
 })
 export class ProductManageComponent implements OnInit {
-  nameProductKeySearch: string;
-  posterKeySearch: string;
+  nameProductKeySearch: '';
+  posterKeySearch: '';
   suggestions: boolean;
   arrProductSuggest: string[];
 
@@ -27,10 +29,14 @@ export class ProductManageComponent implements OnInit {
 
   hasSuggest(sg: boolean): void {
     this.suggestions = sg;
+    this.arrProductSuggest = [];
   }
 
   findSuggest(key: string): void {
     this.arrProductSuggest = [];
+    if (key === undefined) {
+      key = '';
+    }
     if (key !== '') {
       const a = Math.random() * 10;
       for (let i = 0; i < a; i++) {
