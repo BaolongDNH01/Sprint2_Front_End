1aa4b2cb720e60a216502d97d486f89df0aef5b0
tocdovodoi <thanhtungspt@gmail.com>
2020-10-05 11:17:39 +0700

    tung
    1. Xoá app.component.html
    2. Thêm product-details.html
    3.  Thêm product-details.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore'
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
 import {AppRoutingModule} from './app-routing.module';
+import {ProductModule} from './product/product.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AngularFirestoreModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AngularFirestoreModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AppRoutingModule,
+        ProductModule
+    ],
   providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
