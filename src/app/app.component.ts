1c602df6d8f60aebd0f520c094eb06066a540a28
BaolongDNH01 <61400179+BaolongDNH01@users.noreply.github.com>
2020-10-05 22:39:33 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
