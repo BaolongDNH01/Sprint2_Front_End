595d24097b778e3942c9b4bd8d1d5381a7b61d54
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-10-06 15:09:12 +0700

    quan

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -27,4 +27,7 @@ export class UserService {
   sendEmail(user: User): Observable<any>{
     return this.httpClient.post<any>(this.API_URL + '/register', user);
   }
+  getUserByUserName(userName: string): Observable<User>{
+    return this.httpClient.get<User>(this.API_URL + '/getUserByUserName/' + userName);
+  }
 }
