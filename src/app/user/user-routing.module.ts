2254da4841d7fe561e3e17d5eac40e71f879fb1d
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-10-06 09:01:31 +0700

    quan

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -4,12 +4,16 @@ import {RouterModule, Routes} from '@angular/router';
 import {ListUserComponent} from './list-user/list-user.component';
 import {LockUserComponent} from './lock-user/lock-user.component';
 import {AddUserComponent} from './add-user/add-user.component';
+import {SendMailComponent} from './send-mail/send-mail.component';
+import {ActivatedAccountComponent} from './activated-account/activated-account.component';
 
 const routes: Routes = [
   {path: '', children: [
       {path: '', component: ListUserComponent},
       {path: 'lock-user/:ids', component: LockUserComponent},
-      {path: 'add-user', component: AddUserComponent}
+      {path: 'add-user', component: AddUserComponent},
+      {path: 'send-email', component: SendMailComponent},
+      {path: 'activated-account/:token', component: ActivatedAccountComponent}
     ]}
 ];
 
