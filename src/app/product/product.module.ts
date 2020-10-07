1d5e7abe56a08828adac29a44331ff83c94ef167
phuong <daut.phuong@gmail.com>
2020-10-07 10:32:26 +0700

    update

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule, DatePipe} from '@angular/common';
-import { ProductDetailsComponent } from './product-details/product-details.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
 import {AuctionModule} from '../auction/auction.module';
-
+import {ProductCreateComponent} from './product-create/product-create.component';
+import {MatSelectModule} from '@angular/material/select';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
-    declarations: [ProductDetailsComponent],
-    exports: [
-        ProductDetailsComponent
-    ],
+  declarations: [ProductCreateComponent,
+    ProductDetailsComponent],
+  exports: [
+    ProductCreateComponent,
+    ProductDetailsComponent
+  ],
   imports: [
     CommonModule,
-    AuctionModule
+    AuctionModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
-  providers:[DatePipe]
+  providers: [DatePipe]
 })
-export class ProductModule { }
+export class ProductModule {
+}
