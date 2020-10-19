d56695278df0fe21769d013c28e68f41ab47c14a
doquockhanh <khanhquocdo.qt@gmail.com>
2020-10-19 08:18:05 +0700

    khanh fix lặt vặt

--- src/app/home-page/home/home.component.ts
+++ src/app/home-page/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {ProductService} from '../../product/product.service';
+import {Category} from '../../product/category';
 
 @Component({
   selector: 'app-home',
@@ -9,18 +10,19 @@ import {ProductService} from '../../product/product.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  listCategory: Category[];
 
   constructor(
     private router: Router,
     private httpClient: HttpClient,
-    private productService: ProductService
+    private productService: ProductService,
   ) {
   }
 
   ngOnInit(): void {
-
-
+    this.productService.findAllCategoryDto().subscribe(
+      list => this.listCategory = list
+    );
   }
 
 
